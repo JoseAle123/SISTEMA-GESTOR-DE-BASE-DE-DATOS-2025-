3ed4a3a2838b4889c083a5857659563481e3491f
JoseAle123 <jmachacamu@unsa.edu.pe>
2025-04-13 19:17:26 -0500

    Funcion para crear esquemas

--- megatron.cpp
+++ megatron.cpp
@@ -8,6 +8,51 @@
 
 class Tabla {
 public:
+    static void crearEsquema(const std::string& nombreArchivoTabla) {
+        std::ifstream archivo("tablas\\" + nombreArchivoTabla);
+        if (!archivo.is_open()) {
+            std::cerr << "No se pudo abrir la tabla: " << nombreArchivoTabla << std::endl;
+            return;
+        }
+
+        std::string linea;
+        if (!std::getline(archivo, linea)) {
+            std::cerr << "El archivo está vacío." << std::endl;
+            archivo.close();
+            return;
+        }
+
+        archivo.close();
+
+        std::stringstream ss(linea);
+        std::string campo;
+        std::vector<std::string> campos;
+
+        while (std::getline(ss, campo, '#')) {
+            campos.push_back(campo);
+        }
+
+        std::string nombreTabla = nombreArchivoTabla.substr(0, nombreArchivoTabla.find('.'));
+        std::ofstream esquema("esquemas\\esquema.txt", std::ios::app);  // app: para no sobrescribir
+
+        if (!esquema.is_open()) {
+            std::cerr << "No se pudo abrir el archivo de esquema." << std::endl;
+            return;
+        }
+
+        esquema << nombreTabla;
+        for (const auto& c : campos) {
+            std::string tipo;
+            std::cout << "Ingresa el tipo de dato para \"" << c << "\" (por ejemplo: str, int): ";
+            std::cin >> tipo;
+            esquema << "#" << c << "#" << tipo;
+        }
+        esquema << "\n";
+
+        esquema.close();
+        std::cout << "Esquema guardado exitosamente en tablas\\esquema.txt\n";
+    }
+
     static void convertirCSV(const std::string& archivoCSV, const std::string& nombreArchivoSalida) {
         if (_access("tablas", 0) == -1) {
             _mkdir("tablas");
@@ -41,6 +86,7 @@ public:
         salida.close();
 
         std::cout << " Archivo convertido guardado en: tablas\\" << nombreArchivoSalida << "\n";
+        crearEsquema(nombreArchivoSalida);
     }
 
     static void imprimirTabla(const std::string& nombreArchivo) {
@@ -59,7 +105,7 @@ public:
 
         archivo.close();
     }
-    
+
 };
 
 int main() {
@@ -69,7 +115,7 @@ int main() {
         std::cout << "1. Crear una tabla desde archivo CSV\n";
         std::cout << "2. Imprimir una tabla por nombre\n";
         std::cout << "0. Salir\n";
-        std::cout << "Seleccione una opción: ";
+        std::cout << "Seleccione una opcion: ";
         std::cin >> opcion;
 
         std::cin.ignore();  // Limpiar buffer del enter
@@ -92,7 +138,7 @@ int main() {
             std::cout << "Saliendo del programa.\n";
         }
         else {
-            std::cout << "Opción inválida. Intente nuevamente.\n";
+            std::cout << "Opcion inválida. Intente nuevamente.\n";
         }
 
     } while (opcion != 0);
